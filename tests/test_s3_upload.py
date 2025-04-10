a13067c035e6ca823337241b7c410592fbbd2ec1
Lev Vereshchagin <mail@vrslev.com>
2025-04-10 15:20:23 +0300

    Adjust test to generate file content within the upload_file calls

--- tests/test_s3_upload.py
+++ tests/test_s3_upload.py
@@ -40,14 +40,13 @@ class TestS3Upload:
         file_name = faker.file_name()
         file_name_prefix = faker.pystr()
         bucket_name = faker.pystr()
-        file_content = generate_binary_content(faker)
 
         uploaded_file = await S3FilesUploader(
             file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
             s3_client=s3_client_mock,
             s3_bucket_name=bucket_name,
             s3_key_generator=lambda file_context: file_name_prefix + file_context.file_name,
-        ).upload_file(file_name=file_name, file_content=file_content)
+        ).upload_file(file_name=file_name, file_content=generate_binary_content(faker))
 
         assert uploaded_file.s3_path == f"{bucket_name}/{file_name_prefix}{file_name}"
         assert s3_client_mock.put_object.mock_calls[0].kwargs["Key"] == file_name_prefix + file_name
@@ -56,14 +55,12 @@ class TestS3Upload:
         s3_client_mock = mock.AsyncMock()
         file_name = faker.file_name()
         file_original_name_key = faker.pystr()
-        bucket_name = faker.pystr()
-        file_content = generate_binary_content(faker)
 
         await S3FilesUploader(
             file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
             s3_client=s3_client_mock,
-            s3_bucket_name=bucket_name,
+            s3_bucket_name=faker.pystr(),
             s3_metadata_generator=lambda file_context: {file_original_name_key: file_context.file_name},
-        ).upload_file(file_name=file_name, file_content=file_content)
+        ).upload_file(file_name=file_name, file_content=generate_binary_content(faker))
 
         assert s3_client_mock.put_object.mock_calls[0].kwargs["Metadata"] == {file_original_name_key: file_name}
