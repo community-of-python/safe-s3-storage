26442d6c07ffb08b49301e3c4d4be22b64593476
Lev Vereshchagin <mail@vrslev.com>
2025-04-10 17:53:48 +0300

    Update base exception class name for consistency and clarity

--- safe_s3_storage/exceptions.py
+++ safe_s3_storage/exceptions.py
@@ -2,31 +2,31 @@ import dataclasses
 
 
 @dataclasses.dataclass
-class BaseS3SafeStorageError(Exception):
+class BaseError(Exception):
     def __str__(self) -> str:
         return self.__repr__().replace(self.__class__.__name__, "")
 
 
 @dataclasses.dataclass
-class ThreatDetectedError(BaseS3SafeStorageError):
+class ThreatDetectedError(BaseError):
     antivirus_response: bytes
 
 
 @dataclasses.dataclass
-class UnsupportedMimeTypeError(BaseS3SafeStorageError):
+class UnsupportedMimeTypeError(BaseError):
     file_name: str
     mime_type: str
     allowed_mime_types: list[str]
 
 
 @dataclasses.dataclass
-class TooLargeFileError(BaseS3SafeStorageError):
+class TooLargeFileError(BaseError):
     file_name: str
     mime_type: str
     max_size: int
 
 
 @dataclasses.dataclass
-class FailedToConvertImageError(BaseS3SafeStorageError):
+class FailedToConvertImageError(BaseError):
     file_name: str
     mime_type: str
