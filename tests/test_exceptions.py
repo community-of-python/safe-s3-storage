1949b7efc5f69737e661a42e3d47c757c1521804
Lev Vereshchagin <mail@vrslev.com>
2025-04-10 15:21:54 +0300

    Adjust test for ThreatDetectedError and use faker for generating test data

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,9 @@
-from s3_safe_storage.exceptions import BaseS3SafeStorageError
+import faker
+
+from s3_safe_storage.exceptions import ThreatDetectedError
+from tests.conftest import generate_binary_content
 
 
-def test_exception_str() -> None:
-    assert str(BaseS3SafeStorageError()) == "()"
+def test_exception_str(faker: faker.Faker) -> None:
+    antivirus_response = generate_binary_content(faker)
+    assert str(ThreatDetectedError(antivirus_response=antivirus_response)) == f"({antivirus_response=})"
