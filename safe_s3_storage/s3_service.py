ac3f6d9c186ecf7caee9f3af878fde948adaffd8
Lev Vereshchagin <mail@vrslev.com>
2025-04-14 15:09:40 +0300

    Optimize SafeS3FilesService class by adding slots for memory efficiency

--- safe_s3_storage/s3_service.py
+++ safe_s3_storage/s3_service.py
@@ -4,5 +4,5 @@ from safe_s3_storage.s3_read import SafeS3FilesReader
 from safe_s3_storage.s3_upload import SafeS3FilesUploader
 
 
-@dataclasses.dataclass(kw_only=True, frozen=True)
+@dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
 class SafeS3FilesService(SafeS3FilesReader, SafeS3FilesUploader): ...
