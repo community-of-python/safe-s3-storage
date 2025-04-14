c76be2daa3600a34b5d437fc008dcf11a53b732e
Lev Vereshchagin <mail@vrslev.com>
2025-04-14 15:07:46 +0300

    Adding SafeS3FilesReader and SafeS3FilesService to __init__.py and __all__

--- safe_s3_storage/__init__.py
+++ safe_s3_storage/__init__.py
@@ -2,6 +2,8 @@ from safe_s3_storage import exceptions
 from safe_s3_storage.file_validator import FileValidator, ImageConversionFormat, ValidatedFile
 from safe_s3_storage.kaspersky_scan_engine import KasperskyScanEngineClient
 from safe_s3_storage.s3_base import BaseS3Service
+from safe_s3_storage.s3_read import SafeS3FilesReader
+from safe_s3_storage.s3_service import SafeS3FilesService
 from safe_s3_storage.s3_upload import SafeS3FilesUploader, UploadedFile
 
 
@@ -10,6 +12,8 @@ __all__ = [
     "FileValidator",
     "ImageConversionFormat",
     "KasperskyScanEngineClient",
+    "SafeS3FilesReader",
+    "SafeS3FilesService",
     "SafeS3FilesUploader",
     "UploadedFile",
     "ValidatedFile",
