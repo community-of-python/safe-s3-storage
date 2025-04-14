e27bcc0ab9c66d582281c2e5d98089c31a8fe00f
Lev Vereshchagin <mail@vrslev.com>
2025-04-14 15:29:56 +0300

    Add S3 file reader (#3)

--- tests/test_s3_upload.py
+++ tests/test_s3_upload.py
@@ -4,7 +4,8 @@ from unittest import mock
 import faker
 
 from safe_s3_storage.file_validator import FileValidator
-from safe_s3_storage.s3_upload import SafeS3FilesUploader, UploadedFile
+from safe_s3_storage.s3_service import SafeS3FilesService
+from safe_s3_storage.s3_upload import UploadedFile
 from tests.conftest import MIME_OCTET_STREAM, generate_binary_content
 
 
@@ -15,11 +16,10 @@ class TestSafeS3FilesUploader:
         bucket_name: typing.Final = faker.pystr()
         file_content: typing.Final = generate_binary_content(faker)
 
-        uploaded_file: typing.Final = await SafeS3FilesUploader(
+        uploaded_file: typing.Final = await SafeS3FilesService(
             file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
             s3_client=s3_client_mock,
-            bucket_name=bucket_name,
-        ).upload_file(file_name=file_name, file_content=file_content)
+        ).upload_file(bucket_name=bucket_name, file_name=file_name, file_content=file_content)
 
         assert uploaded_file == UploadedFile(
             file_content=file_content,
@@ -42,12 +42,11 @@ class TestSafeS3FilesUploader:
         file_name_prefix: typing.Final = faker.pystr()
         bucket_name: typing.Final = faker.pystr()
 
-        uploaded_file: typing.Final = await SafeS3FilesUploader(
+        uploaded_file: typing.Final = await SafeS3FilesService(
             file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
             s3_client=s3_client_mock,
-            bucket_name=bucket_name,
             s3_key_generator=lambda file_context: file_name_prefix + file_context.file_name,
-        ).upload_file(file_name=file_name, file_content=generate_binary_content(faker))
+        ).upload_file(bucket_name=bucket_name, file_name=file_name, file_content=generate_binary_content(faker))
 
         assert uploaded_file.s3_path == f"{bucket_name}/{file_name_prefix}{file_name}"
         assert s3_client_mock.put_object.mock_calls[0].kwargs["Key"] == file_name_prefix + file_name
@@ -57,11 +56,10 @@ class TestSafeS3FilesUploader:
         file_name: typing.Final = faker.file_name()
         file_original_name_key: typing.Final = faker.pystr()
 
-        await SafeS3FilesUploader(
+        await SafeS3FilesService(
             file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
             s3_client=s3_client_mock,
-            bucket_name=faker.pystr(),
             s3_metadata_generator=lambda file_context: {file_original_name_key: file_context.file_name},
-        ).upload_file(file_name=file_name, file_content=generate_binary_content(faker))
+        ).upload_file(bucket_name=faker.pystr(), file_name=file_name, file_content=generate_binary_content(faker))
 
         assert s3_client_mock.put_object.mock_calls[0].kwargs["Metadata"] == {file_original_name_key: file_name}
