77eb016ff9b58bbcddb55a5c1d73d26e12fbe197
Lev Vereshchagin <mail@vrslev.com>
2025-04-14 16:39:23 +0300

    Allow custom s3 key

--- safe_s3_storage/s3_upload.py
+++ safe_s3_storage/s3_upload.py
@@ -20,16 +20,18 @@ class SafeS3FilesUploader(BaseS3Service):
     s3_key_generator: typing.Callable[[ValidatedFile], str] = lambda file_context: file_context.file_name
     s3_metadata_generator: typing.Callable[[ValidatedFile], typing.Mapping[str, str]] = lambda _file_context: {}
 
-    async def upload_file(self, *, bucket_name: str, file_name: str, file_content: bytes) -> UploadedFile:
+    async def upload_file(
+        self, *, bucket_name: str, file_name: str, file_content: bytes, s3_key: str | None = None
+    ) -> UploadedFile:
         validated_file: typing.Final = await self.file_validator.validate_file(
             file_name=file_name, file_content=file_content
         )
-        s3_key: typing.Final = self.s3_key_generator(validated_file)
+        final_s3_key: typing.Final = s3_key or self.s3_key_generator(validated_file)
 
         await stamina.retry(on=botocore.exceptions.BotoCoreError, attempts=self.max_retries)(self.s3_client.put_object)(
             Body=validated_file.file_content,
             Bucket=bucket_name,
-            Key=s3_key,
+            Key=final_s3_key,
             ContentType=validated_file.mime_type,
             Metadata=self.s3_metadata_generator(validated_file),
         )
@@ -39,5 +41,5 @@ class SafeS3FilesUploader(BaseS3Service):
             file_content=validated_file.file_content,
             file_size=validated_file.file_size,
             mime_type=validated_file.mime_type,
-            s3_path=f"{bucket_name}/{s3_key}",
+            s3_path=f"{bucket_name}/{final_s3_key}",
         )
