e348e6293b12466baec206956a40f533a9f6557c
Lev Vereshchagin <mail@vrslev.com>
2025-04-15 12:29:07 +0300

    Decouple file validation from s3 (#6)

--- safe_s3_storage/__init__.py
+++ safe_s3_storage/__init__.py
@@ -1,20 +1,14 @@
 from safe_s3_storage import exceptions
 from safe_s3_storage.file_validator import FileValidator, ImageConversionFormat, ValidatedFile
 from safe_s3_storage.kaspersky_scan_engine import KasperskyScanEngineClient
-from safe_s3_storage.s3_base import BaseS3Service
-from safe_s3_storage.s3_read import SafeS3FilesReader
-from safe_s3_storage.s3_service import SafeS3FilesService
-from safe_s3_storage.s3_upload import SafeS3FilesUploader, UploadedFile
+from safe_s3_storage.s3_service import S3Service, UploadedFile
 
 
 __all__ = [
-    "BaseS3Service",
     "FileValidator",
     "ImageConversionFormat",
     "KasperskyScanEngineClient",
-    "SafeS3FilesReader",
-    "SafeS3FilesService",
-    "SafeS3FilesUploader",
+    "S3Service",
     "UploadedFile",
     "ValidatedFile",
     "exceptions",
--- tests/test_s3_upload.py
+++ tests/test_s3_upload.py
@@ -2,24 +2,31 @@ import typing
 from unittest import mock
 
 import faker
+import pytest
 
+from safe_s3_storage.exceptions import InvalidS3PathError
 from safe_s3_storage.file_validator import FileValidator
-from safe_s3_storage.s3_service import SafeS3FilesService
-from safe_s3_storage.s3_upload import UploadedFile
+from safe_s3_storage.s3_service import S3Service, UploadedFile
 from tests.conftest import MIME_OCTET_STREAM, generate_binary_content
 
 
-class TestSafeS3FilesUploader:
-    async def test_ok_with_defaults(self, faker: faker.Faker) -> None:
+class TestS3ServiceUpload:
+    async def test_ok_upload(self, faker: faker.Faker) -> None:
         s3_client_mock: typing.Final = mock.AsyncMock()
         file_name: typing.Final = faker.file_name()
         bucket_name: typing.Final = faker.pystr()
         file_content: typing.Final = generate_binary_content(faker)
+        file_original_name_key: typing.Final = faker.pystr()
 
-        uploaded_file: typing.Final = await SafeS3FilesService(
-            file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
-            s3_client=s3_client_mock,
-        ).upload_file(bucket_name=bucket_name, file_name=file_name, file_content=file_content)
+        validated_file: typing.Final = await FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]).validate_file(
+            file_name=file_name, file_content=file_content
+        )
+        uploaded_file: typing.Final = await S3Service(s3_client=s3_client_mock).upload_file(
+            validated_file,
+            bucket_name=bucket_name,
+            object_key=validated_file.file_name,
+            metadata={file_original_name_key: file_name},
+        )
 
         assert uploaded_file == UploadedFile(
             file_content=file_content,
@@ -33,33 +40,42 @@ class TestSafeS3FilesUploader:
             Bucket=bucket_name,
             Key=file_name,
             ContentType=MIME_OCTET_STREAM,
-            Metadata={},
+            Metadata={file_original_name_key: file_name},
         )
 
-    async def test_ok_with_custom_key_generator(self, faker: faker.Faker) -> None:
-        s3_client_mock: typing.Final = mock.AsyncMock()
-        file_name: typing.Final = faker.file_name()
-        file_name_prefix: typing.Final = faker.pystr()
-        bucket_name: typing.Final = faker.pystr()
 
-        uploaded_file: typing.Final = await SafeS3FilesService(
-            file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
-            s3_client=s3_client_mock,
-            s3_key_generator=lambda file_context: file_name_prefix + file_context.file_name,
-        ).upload_file(bucket_name=bucket_name, file_name=file_name, file_content=generate_binary_content(faker))
+class TestS3ServiceRead:
+    async def test_ok_read(self, faker: faker.Faker) -> None:
+        file_content: typing.Final = generate_binary_content(faker)
+        bucket_name, s3_key = faker.pystr(), faker.pystr()
+        s3_client_mock: typing.Final = mock.Mock(
+            get_object=mock.AsyncMock(return_value={"Body": mock.Mock(read=mock.AsyncMock(return_value=file_content))})
+        )
 
-        assert uploaded_file.s3_path == f"{bucket_name}/{file_name_prefix}{file_name}"
-        assert s3_client_mock.put_object.mock_calls[0].kwargs["Key"] == file_name_prefix + file_name
+        read_file: typing.Final = await S3Service(s3_client=s3_client_mock).read_file(s3_path=f"{bucket_name}/{s3_key}")
 
-    async def test_ok_with_custom_metadata_generator(self, faker: faker.Faker) -> None:
-        s3_client_mock: typing.Final = mock.AsyncMock()
-        file_name: typing.Final = faker.file_name()
-        file_original_name_key: typing.Final = faker.pystr()
+        s3_client_mock.get_object.assert_called_once_with(Bucket=bucket_name, Key=s3_key)
+        assert read_file == file_content
 
-        await SafeS3FilesService(
-            file_validator=FileValidator(allowed_mime_types=[MIME_OCTET_STREAM]),
-            s3_client=s3_client_mock,
-            s3_metadata_generator=lambda file_context: {file_original_name_key: file_context.file_name},
-        ).upload_file(bucket_name=faker.pystr(), file_name=file_name, file_content=generate_binary_content(faker))
+    async def test_ok_stream(self, faker: faker.Faker) -> None:
+        file_content_chunks: typing.Final = [
+            generate_binary_content(faker) for _ in range(faker.pyint(min_value=2, max_value=10))
+        ]
+        bucket_name, s3_key = faker.pystr(), faker.pystr()
+        s3_client_mock: typing.Final = mock.Mock(
+            get_object=mock.AsyncMock(
+                return_value={"Body": mock.Mock(read=mock.AsyncMock(side_effect=[*file_content_chunks, ""]))}
+            )
+        )
 
-        assert s3_client_mock.put_object.mock_calls[0].kwargs["Metadata"] == {file_original_name_key: file_name}
+        read_chunks: typing.Final = [
+            one_chunk
+            async for one_chunk in S3Service(s3_client=s3_client_mock).stream_file(s3_path=f"{bucket_name}/{s3_key}")
+        ]
+
+        s3_client_mock.get_object.assert_called_once_with(Bucket=bucket_name, Key=s3_key)
+        assert read_chunks == file_content_chunks
+
+    async def test_fails_to_parse_s3_path(self, faker: faker.Faker) -> None:
+        with pytest.raises(InvalidS3PathError):
+            await S3Service(s3_client=mock.Mock()).read_file(s3_path=faker.pystr())
