c814a472f5b5ef7c76158bb30ba2882d797a4c96
Lev Vereshchagin <mail@vrslev.com>
2025-07-08 13:01:31 +0300

    Add `S3Service.create_file_url()` (#9)

--- safe_s3_storage/s3_service.py
+++ safe_s3_storage/s3_service.py
@@ -1,4 +1,5 @@
 import dataclasses
+import datetime
 import typing
 
 import botocore
@@ -7,7 +8,7 @@ import stamina
 from types_aiobotocore_s3 import S3Client
 from types_aiobotocore_s3.type_defs import GetObjectOutputTypeDef
 
-from safe_s3_storage.exceptions import InvalidS3PathError
+from safe_s3_storage.exceptions import FailedToReplaceS3BaseUrlWithProxyBaseUrlError, InvalidS3PathError
 from safe_s3_storage.file_validator import ValidatedFile
 
 
@@ -30,7 +31,6 @@ class UploadedFile(ValidatedFile):
 class S3Service:
     s3_client: S3Client
     max_retries: int = 3
-    read_chunk_size: int = 70 * 1024
 
     async def upload_file(
         self,
@@ -57,16 +57,45 @@ class S3Service:
 
     async def _retrieve_file_object(self, *, s3_path: str) -> GetObjectOutputTypeDef:
         bucket_name, object_key = _extract_bucket_name_and_object_key(s3_path)
+        # TODO: remove retries, they should be configured in s3 client, add info in readme  # noqa: FIX002, TD002, TD003
         return await stamina.retry(on=botocore.exceptions.BotoCoreError, attempts=self.max_retries)(
             self.s3_client.get_object
         )(Bucket=bucket_name, Key=object_key)
 
-    async def stream_file(self, *, s3_path: str) -> typing.AsyncIterator[bytes]:
+    async def stream_file(self, *, s3_path: str, read_chunk_size: int = 70 * 1024) -> typing.AsyncIterator[bytes]:
         file_object: typing.Final = await self._retrieve_file_object(s3_path=s3_path)
         object_body: typing.Final = file_object["Body"]
-        while one_chunk := await object_body.read(self.read_chunk_size):
+        while one_chunk := await object_body.read(read_chunk_size):
             yield one_chunk
 
     async def read_file(self, *, s3_path: str) -> bytes:
         file_object: typing.Final = await self._retrieve_file_object(s3_path=s3_path)
         return await file_object["Body"].read()
+
+    async def create_file_url(
+        self, *, s3_path: str, display_file_name: str, expires_in: datetime.timedelta, proxy_base_url: str | None = None
+    ) -> str:
+        bucket_name, object_key = _extract_bucket_name_and_object_key(s3_path)
+        expires_in_seconds: typing.Final = round(expires_in.total_seconds())
+        presigned_url: typing.Final = await stamina.retry(
+            on=botocore.exceptions.BotoCoreError, attempts=self.max_retries
+        )(self.s3_client.generate_presigned_url)(
+            "get_object",
+            Params={
+                "Bucket": bucket_name,
+                "Key": object_key,
+                "ResponseContentDisposition": f'inline; filename="{display_file_name}"',
+                "ResponseCacheControl": f"max-age={expires_in_seconds}, public",
+            },
+            ExpiresIn=expires_in_seconds,
+        )
+
+        if proxy_base_url is None:
+            return presigned_url
+        if presigned_url == (
+            presigned_url_without_prefix := presigned_url.removeprefix(self.s3_client.meta.endpoint_url)
+        ):
+            raise FailedToReplaceS3BaseUrlWithProxyBaseUrlError(
+                s3_file_presigned_url=presigned_url, proxy_base_url=proxy_base_url
+            )
+        return proxy_base_url + presigned_url_without_prefix
