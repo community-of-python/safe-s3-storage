0f3efa6060e84b19e63e71587e491d6ce5807195
Lev Vereshchagin <mail@vrslev.com>
2025-07-08 13:11:49 +0300

    Remove S3 retries and recommend configuring them in `S3Client` (#10)

--- safe_s3_storage/s3_service.py
+++ safe_s3_storage/s3_service.py
@@ -2,9 +2,6 @@ import dataclasses
 import datetime
 import typing
 
-import botocore
-import botocore.exceptions
-import stamina
 from types_aiobotocore_s3 import S3Client
 from types_aiobotocore_s3.type_defs import GetObjectOutputTypeDef
 
@@ -30,7 +27,6 @@ class UploadedFile(ValidatedFile):
 @dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
 class S3Service:
     s3_client: S3Client
-    max_retries: int = 3
 
     async def upload_file(
         self,
@@ -40,7 +36,7 @@ class S3Service:
         object_key: str,
         metadata: dict[str, str] | None = None,
     ) -> UploadedFile:
-        await stamina.retry(on=botocore.exceptions.BotoCoreError, attempts=self.max_retries)(self.s3_client.put_object)(
+        await self.s3_client.put_object(
             Body=validated_file.file_content,
             Bucket=bucket_name,
             Key=object_key,
@@ -57,10 +53,7 @@ class S3Service:
 
     async def _retrieve_file_object(self, *, s3_path: str) -> GetObjectOutputTypeDef:
         bucket_name, object_key = _extract_bucket_name_and_object_key(s3_path)
-        # TODO: remove retries, they should be configured in s3 client, add info in readme  # noqa: FIX002, TD002, TD003
-        return await stamina.retry(on=botocore.exceptions.BotoCoreError, attempts=self.max_retries)(
-            self.s3_client.get_object
-        )(Bucket=bucket_name, Key=object_key)
+        return await self.s3_client.get_object(Bucket=bucket_name, Key=object_key)
 
     async def stream_file(self, *, s3_path: str, read_chunk_size: int = 70 * 1024) -> typing.AsyncIterator[bytes]:
         file_object: typing.Final = await self._retrieve_file_object(s3_path=s3_path)
@@ -77,9 +70,7 @@ class S3Service:
     ) -> str:
         bucket_name, object_key = _extract_bucket_name_and_object_key(s3_path)
         expires_in_seconds: typing.Final = round(expires_in.total_seconds())
-        presigned_url: typing.Final = await stamina.retry(
-            on=botocore.exceptions.BotoCoreError, attempts=self.max_retries
-        )(self.s3_client.generate_presigned_url)(
+        presigned_url: typing.Final = await self.s3_client.generate_presigned_url(
             "get_object",
             Params={
                 "Bucket": bucket_name,
